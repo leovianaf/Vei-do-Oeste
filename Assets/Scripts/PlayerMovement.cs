af8941d2c030dfba71a58bdfdbfc71be784bac94
Leovianaf <leonardoviana.nave@gmail.com>
2024-12-06 00:12:39 -0300

    feat: Animations Walk and Only 4 directions

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,18 +7,35 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
     private Vector2 movement;
     private Rigidbody2D rb;
+    private Animator animator;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        if (Mathf.Abs(horizontal) > Mathf.Abs(vertical))
+        {
+            movement.x = horizontal;
+            movement.y = 0;
+        }
+        else
+        {
+            movement.x = 0;
+            movement.y = vertical;
+        }
 
         movement = movement.normalized;
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetBool("isMoving", movement != Vector2.zero);
     }
 
     void FixedUpdate()
