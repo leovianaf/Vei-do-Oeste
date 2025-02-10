63081e70e755a45cb1a4594c44abfff58f76d779
Stella Nazário <stellanacleto@gmail.com>
2025-02-10 11:41:36 -0300

    Add cutscene e creditos

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -4,18 +4,28 @@ public class MenuController : MonoBehaviour
 {
     public GameObject menuPrincipal; // Painel do menu principal
     public GameObject popupControles; // Painel do pop-up de controles
+    public GameObject creditos; // Painel do pop-up de creditos
 
     // Método para abrir o pop-up de controles
     public void AbrirControles()
     {
         menuPrincipal.SetActive(false); // Desativa o menu principal
+        creditos.SetActive(false); // Desativa os creditos
         popupControles.SetActive(true); // Ativa o pop-up de controles
     }
 
     // Método para fechar o pop-up de controles
-    public void FecharControles()
+    public void Fechar()
     {
         popupControles.SetActive(false); // Desativa o pop-up de controles
+        creditos.SetActive(false); // Desativa os creditos
         menuPrincipal.SetActive(true); // Reativa o menu principal
+    }
+
+    public void AbrirCreditos()
+    {
+        popupControles.SetActive(false); // Desativa o pop-up de controles
+        creditos.SetActive(true); // Ativa os creditos
+        menuPrincipal.SetActive(false); // Desativa o menu principal
     }
 }
